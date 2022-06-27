90279e9d9be4657b83e121c08ed4d6c74305bb66
Sisa <sisa.philippines@gmail.com>
2022-06-28 00:34:06 +0800

    Add reactive forms module path.

--- sisa-fe/src/app/components/login/login.component.ts
+++ sisa-fe/src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AbstractControl, FormControl, FormGroup, Validators, FormBuilder } from "@angular/forms";
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
