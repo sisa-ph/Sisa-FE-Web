2badcf26298f54f1abc9da9a3ec21dc5958d82c4
Sisa <sisa.philippines@gmail.com>
2022-06-28 00:32:32 +0800

    Add translation module routing path.

--- sisa-fe/src/app/app-routing.module.ts
+++ sisa-fe/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./components/translation/translation.module').then(
+        (m) => m.TranslationModule
+      ),
+  },
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
