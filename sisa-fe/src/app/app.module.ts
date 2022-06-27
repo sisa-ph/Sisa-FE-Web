6cbb9e36b147bf4f7ce6152ed397a61b0e99b17a
Sisa <sisa.philippines@gmail.com>
2022-06-27 23:02:33 +0800

    Add browser animations module.

--- sisa-fe/src/app/app.module.ts
+++ sisa-fe/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
