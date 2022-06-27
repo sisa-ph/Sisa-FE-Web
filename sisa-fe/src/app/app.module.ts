6a260133b6666b5095cebecf542169d8d311076e
Sisa <sisa.philippines@gmail.com>
2022-06-27 23:02:01 +0800

    Add Http Client Json Module.

--- sisa-fe/src/app/app.module.ts
+++ sisa-fe/src/app/app.module.ts
@@ -1,8 +1,10 @@
 /* Components */
 import { AppComponent } from './app.component';
+
+/* Modules */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TranslationComponent } from './translation/translation.component';
